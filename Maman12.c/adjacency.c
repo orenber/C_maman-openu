088333371747995e6f7d7abb6335f2c6baef592f
orenber <orenber@hotmail.com>
2020-05-06 22:20:43 +0300

    1.add documentation 2.fix opating bug 3.fix table view 4 . add h file data

--- Maman12.c/adjacency.c
+++ Maman12.c/adjacency.c
@@ -1,62 +1,101 @@
+/* ***********************************************************************
+Assembly         :
+Author           : Oren Berkovitch
+ID:              : 043189281
+Created          : 06-05-2020
+
+Last Modified By : Oren Berkovitch
+Last Modified On : 06-05-2020
+***********************************************************************
+<summary> 
+this programe have two part :
+1.the user assingn  value to bolean matrix in size N 
+the user will ask who is the main parent and will match for etch child his parent
+2. the user ask for parent and child input and call to the path function thet will search 
+if ther is path between the child to the parent, 
+in the case ther is path or ther is't the function return TRUE or FALSE,
+</summary>
+**********************************************************************
+*/
+
 #include "data.h"
 
+/* <summary>
+main function - user friendly interface.
+</summary>
+<returns>int.</returns>
+*/
 int main(){
     
-	adjmat matrix;
-	int input=0;
-	char parent[] = " ", child[] = " ";
+	adjmat matrix; 
+	int input=0,
+        parent, child;
+	char str_parent[2],
+		 str_child[2];
 
-	matrix_equal(&matrix,FALSE);
-	build_matrix(&matrix);
-	
+    matrix_equal(&matrix,FALSE);
+	build_matrix(matrix);
+	printf("\n\n________________________\nSearch the path..\n"
+		"print -1 -1 for Exit");
 	while (input != EOF)
 	{
-		printf("\n\nPlease insert Parent :");
-		scanf("%s", parent);
-		printf("\n\nPlease insert Child :");
-		input = scanf("%s", child);
+		printf("\n\nPlease insert parent :");
+		scanf("%d", &parent);
+		/*convert int to string*/
+		sprintf(str_parent, "%d", parent);
+		printf("\nSelected parent : %s", str_parent);
 
-		if ((strcmp(parent, "-1") == 0) && (strcmp(child, "-1") == 0))
+		printf("\nPlease insert child :");
+		input = scanf("%d", &child);
+		/*convert int to string*/
+		sprintf(str_child, "%d", child);
+		printf("\nSelected child : %s", str_child);
+
+		if ((parent == -1) && (child==-1))
 		{
 			break;
 		}
-
-			BOOL c = path(matrix, atoi(parent), atoi(child));
-
-			printf(c ? "true" : "false");
+		    
+		BOOL result = path(matrix, parent, child);
+		printf(result ? "true,"
+			"ther is a path between the parent to the child" :
+			"false, ther is no path ");
 		
 	}
 	getchar();
 	return 0;
 }
 
+/* <summary>
+build_matrix - the function is user intarface to build matrix.
+the user ask for every coulum(child) the proprite parent (row)
+and asigne True in the matrix(row,col)  
+</summary> 
+*/
 void build_matrix(adjmat mat) {
 	
-	int col,
-		row,
-		main_parent;
-
+	int col, /*colum matrix index*/
+		row, /*row matrix index*/
+		main_parent; /* tree main parent*/
 		
-	char text_number[] = { " " },
-		text2[] = { " " };
-	
-	printf("choose one main parent from the range: 0-%1d ", N-1);
-	 
-	  scanf("%s", text_number);
-	  main_parent = atoi(text_number);
-	  print_matrix(mat);
+	printf("\nWelcome !!!\n\nPlease choose one main parent from the range [0-%1d] :", N-1);
+	scanf("%d", &main_parent); 
+	printf("\n");
+
+	print_matrix(mat);
 
 	for (col = 0; col < N; col++) {
         if (col == main_parent)
 		{
+			/* skeep on the main parent sinch he dosent have parent */
 			continue;
 		}
 		else {
 			printf("\nfor the given child: %1d \n"
-				"choose one parent from the list\n ", col);
-		    scanf("%s", text2);
-			/* convert char to int*/
-			row  = atoi(text2);
+				"Please choose one parent from the range [0-%1d] : ",col , N-1);
+		    scanf("%d",&row);
+			printf("\n");
+			/*asige value*/
 			mat[row][col] = TRUE;
 			
 		}
@@ -67,29 +106,33 @@ void build_matrix(adjmat mat) {
 
 }
 
+/* <summary>
+print_matrix -print matrix in any size 
+</summary>
+*/
 void print_matrix(adjmat mat) {
 
 	/* calculate size of array */
-	int row = N, col= N;
+	int row = N,/* row index*/
+		col= N;/* col index*/
 	
-	/* print index colume */
 	printf("\t");
 	for (col = 0; col < N;col++)
-	{
-		printf("   %1d", col);
+	{	/* print index colume */
+		printf("%4d", col);
 
 	}
 	printf("\n\t ");
-	/*print separetor line*/
+	
 	for (col = 0; col < N; col++)
 	{
+		/*print separetor line*/
 		printf("____");
-
 	}
 	printf("\n");
 	for (row = 0; row < N; row++)
 	{
-		printf("\t[");
+		printf("%7d [",row);
 		for (col = 0; col < N; col++)
 		{
 			/*  print matrix values*/
@@ -101,31 +144,37 @@ void print_matrix(adjmat mat) {
 			else if (col == N - 1) {
 				printf(" ");
 			}
-
 		}
-		/*print row index*/
-		printf("] %1d\n",row);
+		printf("]\n");
 	}
-
 	return;
-
 }
 
+/* <summary>
+matrix_equal - assign the same value (BOOL)  for etch element in the matrix
+</summary>
+*/
 void matrix_equal(BOOL *mat, BOOL number) {
 	/* calculate size of array */
-	int numel = N * N;
-	for (; numel >= 0; numel--) 
+	int index = N * N; /* index matrix */
+	for (; index >= 0; index--) 
 	{
-		mat[numel] = number;
-	}
+		mat[index] = number;
+	} 
 
 	return ;
 }
 
+/* <summary>
+path - check if ther is path between the child to the parent, 
+in the case ther is path or ther is't the function return TRUE or FALSE,
+</summary>
+*/
 BOOL path(adjmat mat,int parent,int child) {
 	
-	BOOL isParentFound = FALSE;
-	int row = 0,col = child;
+	BOOL isParentFound = FALSE; /* is ther is path betwen the child and the parent?*/
+	int row = 0, /* row index*/
+		col = child; /*colum index*/
 
 	printf("%d\n", child);
 	if((parent == child)&&(parent<N)){
@@ -133,20 +182,17 @@ BOOL path(adjmat mat,int parent,int child) {
 		return isParentFound;
 	}
 
-	
 	for (col; col < N; ) 
 	{
 		for (row = 0; row <= N; ++row) 
-		{
-			
+		{	
 		 // seek parent 
 			if (mat[row][col] == TRUE) 
-			{   
-				
+			{   	
 				printf("%d\n", row);
 
 				if (row == parent) {
-					// found papa!!
+					// found parent!!
 					isParentFound = TRUE;
 					return isParentFound;
 				}
@@ -155,27 +201,14 @@ BOOL path(adjmat mat,int parent,int child) {
 					col = row;
 					break;
 				}
-			
  			}
 			else if (row == N)
 			{
-				// done searcing papa not found
+				// done searcing parent not found
 				return isParentFound;
 			};
 		};
-
 	};
 
 	return isParentFound;
-
-
 }
-
-
-
-
-
-
-
-
-
--- Maman12.c/data.h
+++ Maman12.c/data.h
@@ -1,10 +1,9 @@
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
 
-#define N 11
+#define N 1
 typedef enum
 {
 	FALSE = 0, TRUE = 1
@@ -13,8 +12,29 @@ typedef enum
 
 typedef BOOL adjmat[N][N];
 
-BOOL path(adjmat mat, int parent, int child);
+/* <summary>
+matrix_equal - assign the same value (BOOL)  for etch element in the matrix
+</summary>
+*/
 void matrix_equal(BOOL *mat, BOOL number);
+
+/* <summary>
+print_matrix -print matrix in any size
+</summary>
+*/
 void print_matrix(adjmat mat);
+
+/* <summary>
+build_matrix - the function is user intarface to build matrix.
+the user ask for every coulum(child) the proprite parent (row)
+and asigne True in the matrix(row,col)
+</summary>
+*/
 void build_matrix(adjmat mat);
- 
+
+/* <summary>
+path - check if ther is path between the child to the parent,
+in the case ther is path or ther is't the function return TRUE or FALSE,
+</summary>
+*/
+BOOL path(adjmat mat, int parent, int child);
