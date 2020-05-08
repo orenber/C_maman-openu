27886b1b7160b1cb6e4ab83b59525e6bbeae9f65
orenber <orenber@hotmail.com>
2020-05-08 04:41:50 +0300

    1.fix bug 5->6 2.  fix documation 3. add input output to documataion 4. fix interface view

--- Maman12.c/adjacency.c
+++ Maman12.c/adjacency.c
@@ -9,11 +9,16 @@ Last Modified On : 06-05-2020
 ***********************************************************************
 <summary> 
 this programe have two part :
-1.the user assingn  value to bolean matrix in size N 
-the user will ask who is the main parent and will match for etch child his parent
-2. the user ask for parent and child input and call to the path function thet will search 
-if ther is path between the child to the parent, 
-in the case ther is path or ther is't the function return TRUE or FALSE,
+
+1.the user assign value to boolean matrix in size NxN
+the user will be asked who is the main parent,
+than the user will match for each child leaf his parent.
+
+2. the user ask for parent and child input and call to the path function that will search
+if there is path between the child to the parent,
+in the case there is path the output return TRUE,
+or there isn't the output return FALSE.
+
 </summary>
 **********************************************************************
 */
@@ -27,39 +32,38 @@ main function - user friendly interface.
 */
 int main(){
     
-	adjmat matrix; 
-	int input=0,
-        parent, child;
-	char str_parent[3],
-		 str_child[3];
+	adjmat matrix; /* boolean Adjacency matrix */
+	int input=0, /* input from the user scanf */ 
+        parent, /* leaf in the tree that connect to child leaf*/
+		child; /* leaf in the tree*/
 
+	/*assign matrix equal to false (zeros) */
     matrix_equal(matrix,FALSE);
+	
+	/* build matrix by the user*/
 	build_matrix(matrix);
-	printf("\n\n________________________\nSearch the path..\n"
-		"print -1 -1 for Exit");
-	while (input != EOF)
-	{
-		printf("\n\nPlease insert parent :");
-		scanf("%d", &parent);
-		/*convert int to string*/
-		sprintf(str_parent, "%2d", parent);
-		printf("Selected parent : %s", str_parent);
+
+	printf("\n\n_____________________________\nSearch the path..\n"
+		"\nPrint -1 Enter -1 Enter for Exit");
+	
+	while (input != EOF){
 
 		printf("\n\nPlease insert child :");
 		input = scanf("%d", &child);
-		/*convert int to string*/
-		sprintf(str_child, "%d", child);
-		printf("Selected child : %s", str_child);
+		printf("Selected child : %d", child);
 
-		if ((parent == -1) && (child==-1))
-		{
+		printf("\n\nPlease insert parent :");
+		scanf("%d", &parent);
+		printf("Selected parent : %d\n", parent);
+
+		if ((parent == -1) && (child==-1)){
 			break;
 		}
 		    
 		BOOL result = path(matrix, parent, child);
 		printf(result ? "\ntrue,"
-			"\nTher is a path between the parent to the child" :
-			"false, \nTher is no path ");
+			"\nTher is a path between the child to the parent" :
+			"\nfalse, \nTher is no path ");
 		
 	}
 
@@ -67,27 +71,33 @@ int main(){
 }
 
 /* <summary>
-build_matrix - the function is user intarface to build matrix.
-the user ask for every coulum(child) the proprite parent (row)
-and asigne True in the matrix(row,col)  
-</summary> 
+build matrix - the function is user interface to build Adjacency matrix.
+the user will be ask for every child the appropriate parent
+and assign boolean value (True) to the matrix.
+</summary>
+<input>
+	adjmat mat - boolean Adjacency matrix in size NxN.
+</input>
 */
 void build_matrix(adjmat mat) {
 	
-	int col, /*colum matrix index*/
+	int col, /*coulum matrix index*/
 		row, /*row matrix index*/
 		main_parent; /* tree main parent*/
 		
-	printf("\nWelcome !!!\n\nPlease choose one main parent from the range [0-%1d] :", N-1);
+	printf("\nWelcome !!!\n\nThis is your adjacency matrix, please "
+		     "feel free to fill the indexes\n" 
+		     "for every child match the appropriate parent\n\n");
+		
+	print_matrix(mat);
+
+	printf("\nPlease choose one main parent from the range [0-%1d] :", N - 1);
 	scanf("%d", &main_parent); 
 	printf("\n");
 
-	print_matrix(mat);
-
 	for (col = 0; col < N; col++) {
-        if (col == main_parent)
-		{
-			/* skeep on the main parent sinch he dosent have parent */
+        if (col == main_parent){
+			/* skip on the main parent since he doesn't have parent */
 			continue;
 		}
 		else {
@@ -95,9 +105,8 @@ void build_matrix(adjmat mat) {
 				"Please choose one parent from the range [0-%1d] : ",col , N-1);
 		    scanf("%d",&row);
 			printf("\n");
-			/*asige value*/
+			/*assign value*/
 			mat[row][col] = TRUE;
-			
 		}
 		 print_matrix(mat);
 	};
@@ -107,8 +116,11 @@ void build_matrix(adjmat mat) {
 }
 
 /* <summary>
-print_matrix -print matrix in any size 
+print_matrix -print Adjacency matrix in size NxN.
 </summary>
+<input>
+	adjmat mat - boolean Adjacency matrix in size NxN..
+</input>
 */
 void print_matrix(adjmat mat) {
 
@@ -117,28 +129,24 @@ void print_matrix(adjmat mat) {
 		col= N;/* col index*/
 	
 	printf("\t");
-	for (col = 0; col < N;col++)
-	{	/* print index colume */
+	for (col = 0; col < N;col++){
+		/* print index colume */
 		printf("%4d", col);
-
 	}
+
 	printf("\n\t ");
 	
-	for (col = 0; col < N; col++)
-	{
+	for (col = 0; col < N; col++){
 		/*print separetor line*/
 		printf("____");
 	}
 	printf("\n");
-	for (row = 0; row < N; row++)
-	{
+	for (row = 0; row < N; row++){
 		printf("%7d [",row);
-		for (col = 0; col < N; col++)
-		{
+		for (col = 0; col < N; col++){
 			/*  print matrix values*/
 			printf(" %2d", mat[row][col]);
-			if (col < N-1)
-			{
+			if (col < N-1){
 				printf(",");
 			}
 			else if (col == N - 1) {
@@ -150,18 +158,21 @@ void print_matrix(adjmat mat) {
 	return;
 }
 
-/* <summary>
-matrix_equal - assign the same value (BOOL)  for etch element in the matrix
+/*<summary>
+matrix equal - assign the same value (BOOL) for each element in the Adjacency matrix .
 </summary>
+<input>
+	adjmat mat - boolean Adjacency matrix in size NxN.
+	BOOL number - boolen value to assige the matrix TRUE(1) or FALSE(0).
+</input>
 */
 void matrix_equal(adjmat mat, BOOL number) {
-	/* calculate size of array */
-	int col = N, 
-		row = N; /* index matrix */
-	for (col = 0; col < N; col++)
-	{
-		for (row = 0; row < N;row++)
-		{
+
+	int col = N, /*coulum index matrix */
+		row = N; /*row index matrix */
+
+	for (col = 0; col < N; col++){
+		for (row = 0; row < N;row++){
 			mat[col][row] = number;
 		}
 	}
@@ -169,47 +180,50 @@ void matrix_equal(adjmat mat, BOOL number) {
 }
 
 /* <summary>
-path - check if ther is path between the child to the parent, 
-in the case ther is path or ther is't the function return TRUE or FALSE,
+path - check if there is path between the child leaf to the parent leaf,
+in the case there is path the function output return TRUE,
+otherwise output is FALSE.
 </summary>
+<input>
+	adjmat mat - boolean Adjacency matrix in size NxN.
+	int parent - leaf in the tree that connect to child leaf.
+	int child - leaf in the tree. 
+</input>
+<output>
+	BOOL isParentFound - is there is path between the child and the parent?.
+</output>
 */
 BOOL path(adjmat mat,int parent,int child) {
 	
-	BOOL isParentFound = FALSE; /* is ther is path betwen the child and the parent?*/
+	BOOL isParentFound = FALSE; /* is there is path between the child and the parent?*/
 	int row = 0, /* row index*/
-		col = child; /*colum index*/
+		col = child; /*coulum index*/
 
-	printf("%d\n", child);
-	if((parent == child)&&(parent<N)){
+	printf("\n%d->", child);
+	if((parent == child)&&(parent<N)&&(parent >= 0)){
+		printf("%d", parent);
 		isParentFound = TRUE;
 		return isParentFound;
 	}
 
-	for (col; col < N; ) 
-	{
-		for (row = 0; row <= N; ++row) 
-		{	
+	for (col; col < N; ) {
+		for (row = 0; row < N; ++row) {	
 		 // seek parent 
-			if (mat[row][col] == TRUE) 
-			{   	
-				printf("%d->", row);
-
+			if (mat[row][col] == TRUE) {   	
 				if (row == parent) {
 					printf("%d", parent);
 					// found parent!!
 					isParentFound = TRUE;
 					return isParentFound;
-
 				}
-				else
-				{
+				else{
+					printf("%d->", row);
 					col = row;
 					break;
 				}
  			}
-			else if (row == N)
-			{
-				// done searcing parent not found
+			else if (row == N-1){
+				// done searching ,parent not found
 				return isParentFound;
 			};
 		};
