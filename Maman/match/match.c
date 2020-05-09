48f6cb77c1f4057089b356db995f9afc1c78a681
oren <orenber@hotmail.com>
2020-05-10 02:36:43 +0300

    Add files via upload
    1. split to folders match , seqeunce
    2. create makefile 
    3. add test
    4. add screen shot

---
+++ Maman/match/match.c
@@ -0,0 +1,141 @@
+/* ***********************************************************************
+   Assembly         : 
+   Author           : Oren Berkovitch
+   Created          : 03-25-2020
+  
+   Last Modified By : Oren Berkovitch
+   Last Modified On : 04-05-2020
+   ***********************************************************************
+<summary>this programs take 2 string inputs from the user:
+text and pattern , it Returns the index location from which the best match occurs
+(between the text to the pattern).
+</summary>
+**********************************************************************
+*/
+
+#include <stdio.h>
+#include <string.h>
+#define MEM 101 /* array storage*/
+
+int match_score(char text[], char pattern[]);
+int match(char text[], char pattern[]);
+
+
+/* <summary>
+main function - user friendly interface.
+ </summary>
+ <returns>int.</returns>
+ */
+
+int main() {
+	
+	int index;/* the best index position match between the pattern to the text */
+	char text[MEM], /*text string */
+		pattern[MEM], /* pattern string */
+	    *input = "";/* input user*/
+
+
+	printf("Welcome !!! \nPress q and Enter to quit\n");
+	while (input != NULL) {
+
+		printf("\n\nPlease insert text: ");
+		input = fgets(text,MEM,stdin);
+		strtok(text, "\n");/* remove trail \n*/
+		printf("\nText is: %s ",text);
+
+		if (strcmp(text, "q") == 0) {
+			break;
+		}
+
+		printf("\n\nPlease insert pattern: ");
+		fgets(pattern, MEM,stdin);
+		strtok(pattern, "\n");/* remove trail \n*/
+		printf("\nPattern is: %s ", pattern);
+
+		index = match(text, pattern);
+
+		printf("\n\nThe best pattern match is in the index %d", index);
+	    
+	}
+	return 0;
+
+}
+
+
+/* <summary>
+ Matches the specified text.
+this function match the patten string  to the text string,
+first it cut the text string to sub string in the same length as the pattern string 
+than it call the match_score function
+that give score for the match between the text sub string to the pattern string, 
+this procedure go on for every sub string in the text from index 0 to loop_size,
+the function return the index position  of the best minimum match score 
+(the score scale is: 0 for perfect match, pattern length - worst match).
+   </summary>
+   <param name="text">The text string .</param>
+   <param name="pattern">The pattern string .</param>
+   <returns>int index for the min match score.</returns>
+ */
+int match(char text[], char pattern[]) {
+	
+	int score_match,/* score for the match between the pattern to text */
+		pattern_length = (int)strlen(pattern), /*pattern string length */
+		best_min_score = pattern_length, /* min score for all the subs string */
+		index_match_position = 0,/* index best match position */
+		loop_size = (int)strlen(text) - pattern_length; /*loop size iteration*/
+
+	char sub_string[MEM]; /* partial string of text string */
+	int i;
+
+	for (i = 0; i <= loop_size; ++i) {
+		/* sub string */
+		strncpy(sub_string,text+ i, pattern_length) ;
+
+		/* give score for the match */
+		score_match = match_score(sub_string, pattern);
+
+		if (score_match < best_min_score){
+			/* take the best score of all iteration sub string (minimum score)*/
+			best_min_score = score_match;
+			index_match_position = i;
+		
+			if (best_min_score == 0) {
+				/* in the case their is perfect match
+				their is no need for more search */
+				break;
+
+			}
+		}
+
+	}
+	return index_match_position;
+
+}
+
+
+/* <summary>
+    Matches score.
+    it compare the pattern string to the text string (the same length)
+	for every latter , if their is a un match between
+	the text latter to the pattern latter
+	it give score for every latter that un match. 
+	the function return the score, (how much latter in the pattern not found in  the text).
+	*/
+/* </summary>
+   <param name="text">The text string.</param>
+   <param name="pattern">The pattern string.</param>
+   <returns>int. return the un match score  </returns>
+ */
+int match_score(char text[], char pattern[]) {
+
+	int score_un_match = 0,/* score for the match between the pattern to text*/
+		pattern_length = (int)strlen(pattern); /*pattern string length*/
+	int j;
+	for ( j = 0; j < pattern_length; ++j) {
+
+		if (text[j]!=pattern[j]) {
+			score_un_match++;
+		}
+	}
+	return score_un_match;
+}
---
+++ Maman/sequence/sequence.c
@@ -0,0 +1,180 @@
+/***********************************************************************
+ Assembly         : 
+ Author           : Oren Berkovitch
+ Created          : 03-25-2020
+ Last Modified By : Oren Berkovitch
+ Last Modified On : 04-03-2020
+ ***********************************************************************
+ <copyright file="sequence.c" company="">
+     Copyright (c) . All rights reserved.
+ </copyright>
+ <summary>
+ this program get string from the user and check 
+ if the ascii latter that contain the string is in:
+1.  Ascending order
+2.  Monotonic ascending order
+3.  Descending order
+4.  Monotonic descending order
+5.  Un order
+</summary>
+ ***********************************************************************
+*/
+
+#include <stdio.h>
+#include <string.h>
+#define MEM 101 /* array storage*/
+ 
+typedef enum
+{
+	FALSE = 0, TRUE = 1
+
+}BOOL;
+
+typedef enum
+{
+    UP=1, DOWN=-1, CENTER=0
+
+}DIRECTION;
+
+void f_sequence(char text[],char* str);
+void direction_dictionary(DIRECTION flag, BOOL real, char* str);
+
+/* <summary>
+ main function - user friendly interface.
+ </summary>
+ <returns>int.</returns>
+ */
+int main()
+{
+	char str_direction[MEM] = "",/* string direction*/
+		text[MEM]; /* text input */
+		int input =0; 
+
+    printf("Welcome !!! \nPress q and Enter to quit\n");
+	while (input!=EOF) {
+
+		
+		printf("\n\nPlease insert text :");
+		input = scanf("%s",text);
+		if (strcmp(text, "q") == 0) {
+			break;
+		}
+
+		f_sequence(text, str_direction);
+		printf("\nThe string '%s' is in: %s ",text, str_direction);
+
+		getchar();
+	}
+	return 0;
+}
+
+/* <summary>
+ fs sequence.
+ this function check the the direction of the string
+ if the ascii latter that contain the string is in:
+1.  Ascending order
+2.  Monotonic ascending order
+3.  Descending order
+4.  Monotonic descending order
+5.  Un order
+ 
+ it Checks the direction of each letter pair
+ relative to the previous letter pair 
+ for the whole string 
+ </summary>
+ <param name="text">The text input.</param>
+ <param name="*string_direction">string_direction.</param>
+*/
+void f_sequence(char text[], char *string_direction) {
+	
+        int text_length = (int)strlen(text); /* text length */
+        	DIRECTION previous_direction= CENTER, /* direction of previous pair char*/
+                          current_direction;/* direction of pair char */
+        BOOL is_monotonic = TRUE;/*is the string is monotonic Series? */
+        
+        int i;
+        strcpy(string_direction, ""); /*initialize string*/        
+	
+	for ( i = 1; i < text_length; ++i) {
+		  
+
+		if (text[i - 1]>text[i]) {
+			current_direction = DOWN;
+			if (previous_direction==UP) {
+				strcpy(string_direction,"un_order");
+				/* in the case the string is un order
+				their is no need for more search - get out*/
+				break;
+			}
+		}
+		else if (text[i - 1] < text[i]) {
+			current_direction = UP;
+			if (previous_direction==DOWN) {
+				strcpy(string_direction,"un_order");
+				/* in the case the string is un order
+				their is no need for more search - get out*/
+				break;
+			}
+		}
+		else if (text[i - 1] == text[i]) {
+			is_monotonic = FALSE;
+			if (previous_direction==DOWN) {
+				current_direction = DOWN;
+			}
+			else if (previous_direction==UP) {
+				current_direction = UP;
+			}
+			else {
+
+				current_direction = CENTER;
+			}
+		}
+
+		previous_direction = current_direction;
+
+	}
+	 
+		direction_dictionary(current_direction, is_monotonic, string_direction);
+ 
+		return;
+}
+
+/* <summary>
+ Directions  dictionary.
+	this function get the Direction of the last pair letters direction
+	and monotonic parameter and return the whole string direction 
+ </summary>
+    <param name="dir">The direction of the last pair chars.</param>
+    <param name="is_monotonic">is the string is monotonic.</param>
+	<param name="*string_direction">string direction.</param>
+*/
+void direction_dictionary(DIRECTION dir, BOOL is_monotonic,char *string_direction) {
+  
+	if (strcmp(string_direction, "un_order") == 0)
+	{
+		return;
+	}
+
+	if (dir == DOWN) {
+		if (is_monotonic == TRUE) {
+			strcpy(string_direction, "monotonic descending order");
+		}
+		else {
+			strcpy(string_direction, "descending order");
+		}
+	}
+	else if (dir == UP) {
+		if (is_monotonic == TRUE) {
+			strcpy(string_direction, "monotonic ascending order");
+		}
+		else {
+			strcpy(string_direction, "ascending order");
+		}
+	}
+	else if (dir == CENTER) {
+		strcpy(string_direction, "ascending order");
+	}
+
+	return;
+}
+
