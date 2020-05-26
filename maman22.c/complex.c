ec228a46c2217bdcd7b290ce101496ce57aa6a39
orenber <orenber@hotmail.com>
2020-05-26 06:55:05 +0300

    add setCommand  - function thet separet the input from the user to inputs

--- maman22.c/complex.c
+++ maman22.c/complex.c
@@ -1,65 +1,100 @@
 #include "complex.h"
 
 
-complex read_comp(complex comp, double real, double img) {
+Complex* read_comp(Complex comp, double real, double img) {
 	comp.real = real;
 	comp.img = img;
-	return comp;
+	return &comp;
 }
 
-void print_comp(complex comp) {
+void print_comp(Complex comp) {
 
-	printf("%1f+(%1f)i", comp.real, comp.img);
+	printf("%1f + (%1f)i\n", comp.real, comp.img);
 	return ;
 }
 
-complex add_comp(complex comp1, complex comp2) {
-	complex result;
+Complex add_comp(Complex comp1, Complex comp2) {
+	Complex result;
 
 	result.real = comp1.real + comp2.real;
 	result.img = comp1.img + comp2.img;
 	return result;
 }
 
-complex sub_comp(complex comp1, complex comp2) {
-	complex result;
+Complex sub_comp(Complex comp1, Complex comp2) {
+	Complex result;
 
 	result.real = comp1.real - comp2.real;
 	result.img = comp1.img - comp2.img;
 	return result;
 }
 
-complex mult_comp_real(complex comp1, double real) {
-	complex result;
+Complex mult_comp_real(Complex comp1, double real) {
+	Complex result;
 
 	result.real = real*comp1.real ;
 	result.img = real*comp1.img;
 	return result;
 }
 
-complex mult_comp_img(complex comp1, double img) {
-	complex result;
+Complex mult_comp_img(Complex comp1, double img) {
+	Complex result;
 
 	result.real = comp1.img*img*-1 ;
 	result.img =  comp1.real*img;
 	return result;
 }
 
-complex mult_comp_comp(complex comp1, complex comp2) {
-	complex result;
+Complex mult_comp_comp(Complex comp1, Complex comp2) {
+	Complex result;
 
 	result.real = comp1.real*comp2.real-comp1.img*comp2.img;
 	result.img = comp1.real*comp2.img +comp2.real* comp1.img;
 	return result;
 }
 
-double abs_comp(complex comp1) {
+double abs_comp(Complex comp1) {
 	
 	double absValue;
 	
 	absValue = sqrt(pow(comp1.real, 2) - pow(comp1.img,2));
 	
 	return absValue;
+}
+
+
+Complex getComplex(char complexName) {
+
+	Complex comp;
+
+	switch (complexName) {
+
+	case 'A':
+		comp = A;
+		break;
+
+	case 'B':
+		comp = B;
+		break;
+
+	case 'C':
+		comp = C;
+		break;
+
+	case 'D':
+		comp = D;
+		break;
+
+	case 'E':
+		comp = E;
+		break;
+
+	case 'F':
+		comp = F;
+		break;
+	}
+	return comp;
+
 }
 
 
--- maman22.c/complex.h
+++ maman22.c/complex.h
@@ -7,26 +7,39 @@
 typedef struct {
 	double real;
 	double img;
-}complex;
+}Complex;
 
-complex A, B, C, D, E, F;
+Complex A, B, C, D, E, F;
 
-complex read_comp(complex comp, double real, double img);
+/*struct {
+	char name;
+	Complex *comp;
+}compexDictionary[] = {
+	{'A',&A},{'B',&B },{ 'C',&C },{'D',&D },{ 'E',&E },{ 'F',&F }
+};*/
 
-void print_comp(complex comp);
+Complex* read_comp(Complex comp, double real, double img);
 
-complex add_comp(complex comp1, complex comp2);
+void print_comp(Complex comp);
 
-complex sub_comp(complex comp1, complex comp2);
+Complex add_comp(Complex comp1, Complex comp2);
 
-complex mult_comp_real(complex comp1, double real);
+Complex sub_comp(Complex comp1, Complex comp2);
 
-complex mult_comp_img(complex comp1, double img);
+Complex mult_comp_real(Complex comp1, double real);
 
-double abs_comp(complex comp1);
+Complex mult_comp_img(Complex comp1, double img);
+
+Complex mult_comp_comp(Complex comp1, Complex comp2);
+
+double abs_comp(Complex comp1);
 
 void stop(void);
 
-void setCommand(char command[]);
+void setCommand(char command[], char seperator[]);
+
+Complex getComplex(char complexName);
+
+
 
 
