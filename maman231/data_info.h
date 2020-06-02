7cd3b79155b7c3b60d0c38e9d121c4415e03ffce
orenber <orenber@hotmail.com>
2020-06-02 12:24:27 +0300

    build dictionary for numbers

---
+++ maman231/data_info.h
@@ -0,0 +1,11 @@
+ 
+
+#include <stdio.h>
+#include <string.h>
+
+#define MEM 30
+
+void dictionary_number(char number[], char number_string[]);
+
+void twenties_dictionary(char number, char number_string[]);
+
