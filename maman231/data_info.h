98a8af66b1ed4b11d831f5826f8b9dff71a3835c
orenber <orenber@hotmail.com>
2020-06-03 16:54:04 +0300

    add data file

--- maman231/data_info.h
+++ maman231/data_info.h
@@ -5,6 +5,12 @@
 
 #define MEM 30
 
+typedef enum
+{
+	False = 0, True = 1
+
+}BOOL;
+
 void dictionary_number(char number[], char number_string[]);
 
 void twenties_dictionary(char number, char number_string[]);
